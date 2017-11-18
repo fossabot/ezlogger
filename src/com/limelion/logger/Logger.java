377cd1addaa2ff2df5e6e6600c08824a4d9cc7ff
LimeiloN <memorial.limelion@gmail.com>
2017-11-18 17:52:30 +0100

    Update

--- src/com/limelion/logger/Logger.java
+++ src/com/limelion/logger/Logger.java
@@ -1,20 +1,24 @@
-/*
-Copyright 2017 LimelioN (github.com/LimeiloN)
-
-Permission is hereby granted, free of charge, 
-to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-to deal in the Software without restriction, including without limitation the rights to use, copy, modify, 
-merge, publish, distribute, sublicense, and/or sell copies of the Software, 
-and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
-WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+/*******************************************************************************
+ * Copyright (C) 2017 LimelioN
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ ******************************************************************************/
 package com.limelion.logger;
 
 import java.io.File;
@@ -23,6 +27,8 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.fusesource.jansi.AnsiConsole;
 
 public class Logger {
 
@@ -35,6 +41,7 @@ public class Logger {
 	private boolean debug = false;
 	private boolean toConsole = true;
 	private boolean toFile = true;
+	private boolean allowColors;
 
 	private Date d;
 	private SimpleDateFormat sdf;
@@ -159,5 +166,11 @@ public class Logger {
 
 	public void setDebug(boolean debug) {
 		this.debug = debug;
+	}
+
+	public void close() {
+		pw.close();
+		pw = null;
+		AnsiConsole.systemUninstall();
 	}
 }
